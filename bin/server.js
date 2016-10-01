1b2b9f9000dd7a459f391ef8c5aad8e01924843c
Matthew Broatch <mnbroatch@gmail.com>
2016-09-30 20:44:04 -0700

    deployable

--- bin/server.js
+++ bin/server.js
@@ -1,20 +1,15 @@
-/*  eslint-disable no-console */
+/*  eslint-disable no-console  */
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const webpack = require('webpack');
-const hotMiddleware = require('webpack-hot-middleware');
-const devMiddleware = require('webpack-dev-middleware');
-const config = require('../webpack.config');
-
-const api = require('./api');
-
 const PORT = process.env.PORT || 3000;
 const build = process.env.NODE_ENV || 'development';
 const app = express();
+
+const api = require('./api');
 
 
 app.use(morgan('dev'));
@@ -22,21 +17,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-if (build === 'development') {
-  const compiler = webpack(config);
-  app.use(hotMiddleware(compiler));
-  app.use(devMiddleware(
-    compiler,
-    {
-      noInfo: true,
-      publicPath: config.output.publicPath,
-    })
-  );
-}
-
 app.use('/api', api);
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, (err) => {
--- prodTools/server.js
+++ prodTools/server.js
@@ -5,12 +5,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const compiler = webpack(config);
-const webpack = require('webpack');
-const hotMiddleware = require('webpack-hot-middleware');
-const devMiddleware = require('webpack-dev-middleware');
-const config = require('../webpack.config');
-
 const PORT = process.env.PORT || 3000;
 const build = process.env.NODE_ENV || 'development';
 const app = express();
@@ -23,18 +17,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-app.use(hotMiddleware(compiler));
-app.use(devMiddleware(
-  compiler,
-  {
-    noInfo: true,
-    publicPath: config.output.publicPath,
-  })
-);
-
 app.use('/api', api);
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(PORT, (err) => {
